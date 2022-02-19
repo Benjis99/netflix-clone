3dfffe4d5f881feba5db5a0228d1ef2bfca611bc
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-02-19 17:15:01 +0100

    Method for netflix nav is done with event

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
